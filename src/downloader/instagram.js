0011faf6101eb0af5feb2991698558d246049053
QwesHX <149233692+QwesHX@users.noreply.github.com>
2024-02-09 09:23:18 +0700

    Update instagram.js
    Sus banget

--- src/downloader/instagram.js
+++ src/downloader/instagram.js
@@ -1,64 +1,87 @@
 const got = require('got');
 const cheerio = require('cheerio');
-const result = []
-
 
 function ig(url) {
-	return new Promise(async(resolve, reject) => {
-		const req = await got.post("https://snapinsta.app/action.php", {
-			form: {
-				url,
-				action: "post",
-			},
-			headers: {
-				origin: "https://snapinsta.app",
-				referer: "https://snapinsta.app/",
-				"user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36",
-				"cookie": "PHPSESSID=g35tdub8ko7vgtu00kqfkr3r3u"
-			},
-		}).text();
-		const $ = cheerio.load(req)
-		$('.row.download-box > div').each(function() {
-			let url = $(this).find(".download-items__btn > a[href]").attr("href");
-			result.push(url)
-		})
-		resolve(result)
-	})
+  const requestData = {
+    q: q,
+    // lain sesuai kebutuhan
+  };
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Accept': '*/*',
+      'X-Requested-With': 'XMLHttpRequest',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.98 Mobile Safari/537.36',
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    got.post('https://snapinsta.to/api/ajaxSearch', {
+      form: requestData,
+      headers: config.headers,
+    }).then(response => {
+      const x = JSON.parse(response.body).data;
+      const $ = cheerio.load(x);
+      const filter1 = $('.download-items .download-items__btn').map((index, item) => {
+        const dataS = $(item).find('a').attr('href');
+        return dataS;
+      }).get();
+
+      console.log('Filtered Data:', filter1);
+      resolve(filter1);
+    }).catch(error => {
+      console.error(error);
+      reject(error);
+    });
+  });
 }
-
-
 
 function igS(username) {
-	return new Promise(async(resolve, reject) => {
-		const reqUrl = await got.get("https://www.instagramsave.com/instagram-story-downloader.php", {
-			headers: {
-				'cookie': 'PHPSESSID=mokpnu09ofun2kohic8tshm3nh',
-				'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36'
-			}
-		}).text();
-		const $ = cheerio.load(reqUrl)
-		let payload = {
-			url: "https://www.instagram.com/" + username,
-			action: 'story',
-			token: $('#token').attr('value'),
-			json: ""
-		}
-		const req = await got.post("https://www.instagramsave.com/system/action.php", {
-			headers: {
-				"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-				origin: "https://www.instagramsave.com",
-				referer: "https://www.instagramsave.com/instagram-story-downloader.php",
-				'cookie': 'PHPSESSID=mokpnu09ofun2kohic8tshm3nh',
-				'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36'
-			},
-			form: payload
-		}).json()
-		resolve(req)
-	})
-}
+  const requestData = {
+    q: `https://www.instagram.com/stories/${q}`, // sesuaikan dengan kebutuhan
+  };
 
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Accept': '*/*',
+      'X-Requested-With': 'XMLHttpRequest',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.98 Mobile Safari/537.36',
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    got.post('https://snapinsta.to/api/ajaxSearch', {
+      form: requestData,
+      headers: config.headers,
+    }).then(response => {
+      const x = JSON.parse(response.body).data;
+      const $ = cheerio.load(x);
+
+      const filter1 = $('.download-items .download-items__btn').map((index, item) => {
+        const dataS = $(item).find('a').attr('href');
+        return dataS;
+      }).get();
+
+      if (filter1.length > 1) {
+        console.log('Filtered Data:', filter1);
+        resolve(filter1);
+      } else if (filter1.length === 1) {
+        console.log('Filtered Data:', filter1[0]);
+        resolve(filter1[0]);
+      } else {
+        console.log('No Data Founded');
+        resolve(null);
+      }
+    }).catch(error => {
+      console.error(error);
+      reject(error);
+    });
+  });
+}
 
 module.exports = {
-	ig,
-	igS
-}
+  ig,
+  igS
+};
